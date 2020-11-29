21c0909f0272d40663ec56b9b7bd2f893111c1da
Zahidkaash <zahidkaash102@gmail.com>
2020-11-29 14:51:02 +0500

    Recursive

---
+++ recursive.java
@@ -0,0 +1,16 @@
+// recursive implementation
+public class recursive 
+{
+
+    public static int gcd(int p, int q) 
+{
+        if (q == 0) return p;
+        else return gcd(q, p % q);
+    }
+ public static void main(String[] args) {
+        int p = Integer.parseInt(args[0]);
+        int q = Integer.parseInt(args[1]);
+        int d  = gcd(p, q);
+        StdOut.println("gcd(" + p + ", " + q + ") = " + d);
+    }
+}
